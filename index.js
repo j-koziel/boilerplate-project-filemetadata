a3267c2690efa0e994ce6e108f8be4223b3174b0
j-koziel <jonathankoziel1@gmail.com>
2024-08-12 08:55:35 +0100

    feat: allowing file uploads and sending analysis

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res, next) => {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
+  if (!req.file) {
+    res.status(400).json({ error: "please upload a file" });
+    next();
+  }
 
+  const { originalname, mimetype, size } = req.file;
 
+  res.status(200).json({ name: originalname, type: mimetype, size });
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
 });
